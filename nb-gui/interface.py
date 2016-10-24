2be66bba0953abe469cd053e7d3bda364ef7c644
Hiranmayi Duvvuri <hiranmayid8@gmail.com>
2016-10-23 17:01:10 -0700

    notebook gui classes

---
+++ nb-gui/interface.py
@@ -0,0 +1,9 @@
+import pytc
+import plotly.plotly as py
+import ipywidgets as widgets
+
+from IPython.display import display, clear_output
+from IPython.html.widgets import interact, interactive
+from plotly.widgets import GraphWidget
+import seaborn as sns
+import glob
